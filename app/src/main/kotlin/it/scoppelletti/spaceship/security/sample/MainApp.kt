8e3ba53fa3fcf732118d952f8606afbb19377294
Dario Scoppelletti <dario@scoppelletti.it>
2020-11-23 16:28:43 +0100

    Minor fix
    http://issuetracker.google.com/issues/173805896

--- app/src/main/kotlin/it/scoppelletti/spaceship/security/sample/MainApp.kt
+++ app/src/main/kotlin/it/scoppelletti/spaceship/security/sample/MainApp.kt
@@ -2,14 +2,15 @@ package it.scoppelletti.spaceship.security.sample
 
 import android.app.Application
 import com.jakewharton.threetenabp.AndroidThreeTen
+import it.scoppelletti.spaceship.html.inject.HtmlComponent
+import it.scoppelletti.spaceship.html.inject.HtmlComponentProvider
 import it.scoppelletti.spaceship.inject.AppComponent
-import it.scoppelletti.spaceship.inject.AppComponentProvider
 import it.scoppelletti.spaceship.inject.StdlibComponent
 import it.scoppelletti.spaceship.security.sample.inject.DaggerSampleComponent
 import it.scoppelletti.spaceship.security.sample.inject.SampleComponent
 
 @Suppress("unused")
-class MainApp : Application(), AppComponentProvider {
+class MainApp : Application(), HtmlComponentProvider {
 
     private lateinit var _sampleComponent: SampleComponent
 
@@ -22,6 +23,8 @@ class MainApp : Application(), AppComponentProvider {
     }
 
     override fun appComponent(): AppComponent = _sampleComponent
+
+    override fun htmlComponent(): HtmlComponent = _sampleComponent
 
     override fun stdlibComponent(): StdlibComponent = _sampleComponent
 }
--- app/src/main/kotlin/it/scoppelletti/spaceship/security/sample/inject/SampleComponent.kt
+++ app/src/main/kotlin/it/scoppelletti/spaceship/security/sample/inject/SampleComponent.kt
@@ -3,13 +3,14 @@ package it.scoppelletti.spaceship.security.sample.inject
 import android.app.Application
 import dagger.BindsInstance
 import dagger.Component
+import it.scoppelletti.spaceship.html.inject.HtmlComponent
 import it.scoppelletti.spaceship.inject.AppComponent
 import it.scoppelletti.spaceship.inject.StdlibComponent
 import javax.inject.Singleton
 
 @Singleton
 @Component(modules = [ SampleModule::class ])
-interface SampleComponent : AppComponent, StdlibComponent {
+interface SampleComponent : AppComponent, HtmlComponent, StdlibComponent {
 
     @Component.Factory
     interface Factory {
